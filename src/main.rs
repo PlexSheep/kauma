03762c14539e4bf9ee8d10a3471be99bc051b1ee
PlexSheep <software@cscherr.de>
2024-10-18 17:18:58 +0200

    refactor(main): remove debug prints

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,6 @@ use serde_json::Value;
 use uuid::Uuid;
 
 fn main() -> Result<()> {
-    #[cfg(debug_assertions)]
-    {
-        eprintln!(
-            "? Example Testcase\n{:#}",
-            serde_json::to_string(&Testcase::default())
-                .expect("could not serialize testcase struct")
-        );
-
-        let mut exhm = ManyTestcases::new();
-        exhm.insert(Uuid::default(), Testcase::default());
-        eprintln!(
-            "? Example ManyTestcases\n{:#}",
-            serde_json::to_string(&exhm).expect("could not serialize testcase struct")
-        )
-    }
-
     let args: Vec<_> = std::env::args().collect();
     if args.len() != 2 {
         eprintln!("! No JSON file was provided for the chalenge definition");
@@ -36,7 +20,6 @@ fn main() -> Result<()> {
     let json_value: Value = serde_json::from_str(&raw_text).inspect_err(|e| {
         eprintln!("! Could not parse the text of the challenge definition file as JSON: {e}")
     })?;
-    eprintln!("? challenge definition: {json_value:#}");
 
     // print our response to stdout
     println!(
