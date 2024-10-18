dbc5dca0a8c1560aa58b4908967aae130243ffe7
PlexSheep <software@cscherr.de>
2024-10-18 14:24:13 +0200

    refactor(main): better error handling in main function

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 use std::path::PathBuf;
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use serde_json::Value;
 
 fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     if args.len() != 2 {
         eprintln!("! No JSON file was provided for the chalenge definition");
+        return Err(anyhow!("no JSON file provided"));
     }
     let path: PathBuf = args[1].clone().into();
     eprintln!("* Path of the challenge definition: {:?}", path);
     eprintln!("* Reading the challenge definition into memory");
 
-    let json_value: Value = serde_json::from_str(&std::fs::read_to_string(&path)?)
-        .expect("challenge definition contains invalid JSON");
-    eprintln!("? Dump of challenge: {json_value:#}");
+    let raw_text = std::fs::read_to_string(&path)
+        .inspect_err(|e| eprintln!("! Could not read the challenge definition file: {e}"))?;
+    let json_value: Value = serde_json::from_str(&raw_text).inspect_err(|e| {
+        eprintln!("! Could not parse the text of the challenge definition file as JSON: {e}")
+    })?;
+    eprintln!("? challenge definition: {json_value:#}");
 
     kauma_analyzer::challenge::run_challenge(&json_value)?;
 
