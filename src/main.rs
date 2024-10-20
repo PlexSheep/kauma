f4a6f6d338972fb86b751d2e6bb3c4c98e263e51
PlexSheep <software@cscherr.de>
2024-10-20 16:05:02 +0200

    refactor(cli): fix clippy unneeded late init of raw_text

--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,21 @@ fn main() -> Result<()> {
         return Err(anyhow!("no JSON file provided"));
     }
 
-    let raw_text: String;
-
-    if args.len() == 2 && args[1] == "-" {
+    let raw_text: String = if args.len() == 2 && args[1] == "-" {
         eprintln!("? Reading from stdin");
         let mut buf: String = String::new();
         let _len = std::io::stdin().read_to_string(&mut buf).inspect_err(|e| {
             eprintln!("! Could not read the challenge definition from stdin: {e}");
         })?;
-        raw_text = buf;
+        buf
     } else {
         let path: PathBuf = args[1].clone().into();
         eprintln!("* Path of the challenge definition: {:?}", path);
         eprintln!("* Reading the challenge definition into memory");
 
-        raw_text = std::fs::read_to_string(&path)
-            .inspect_err(|e| eprintln!("! Could not read the challenge definition file: {e}"))?;
-    }
+        std::fs::read_to_string(&path)
+            .inspect_err(|e| eprintln!("! Could not read the challenge definition file: {e}"))?
+    };
     let json_value: Value = serde_json::from_str(&raw_text).inspect_err(|e| {
         eprintln!("! Could not parse the text of the challenge definition file as JSON: {e}")
     })?;
