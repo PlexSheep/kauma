57e19a1068c0f6dc9b9614faa41ad07db8605ea3
PlexSheep <software@cscherr.de>
2024-10-20 19:22:11 +0200

    feat(common): encode and decode bytes to and from hex

--- src/common/interface.rs
+++ src/common/interface.rs
@@ -1,7 +1,33 @@
 //! Helps parse a few common datatypes from the JSON challenge definitions and write them back out
 
+use std::fmt::Write;
+
 use anyhow::{anyhow, Result};
 use base64::prelude::*;
+
+///  Hex encoded [String] to [byte](u8) slice
+pub fn decode_hex(s: &str) -> Result<Vec<u8>, std::num::ParseIntError> {
+    let mut s: String = s.to_string();
+    if s.starts_with("0x") {
+        s = s.strip_prefix("0x").unwrap().into();
+    }
+    if s.len() % 2 == 1 {
+        s = format!("0{s}");
+    }
+    (0..s.len())
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+        .collect()
+}
+
+/// [Byte](u8) slice to hex encoded [String]
+pub fn encode_hex(bytes: &[u8]) -> String {
+    let mut s = String::with_capacity(bytes.len() * 2);
+    for &b in bytes {
+        write!(&mut s, "{:02x}", b).unwrap();
+    }
+    s
+}
 
 /// Convert the base64 string of the JSON challenge definition to a [`Vec<u8>`].
 ///
@@ -21,4 +47,47 @@ pub fn get_bytes(args: &serde_json::Value, key: &str) -> Result<Vec<u8>> {
 #[inline]
 pub fn put_bytes(data: &Vec<u8>) -> Result<serde_json::Value> {
     Ok(BASE64_STANDARD.encode(data).into())
+}
+
+#[cfg(test)]
+mod test {
+    use crate::common::interface::{decode_hex, encode_hex};
+
+    #[test]
+    fn test_decode_hex() {
+        assert_eq!(decode_hex("0x1337").unwrap(), &[0x13, 0x37]);
+        assert_eq!(decode_hex("1337").unwrap(), &[0x13, 0x37]);
+        assert_eq!(decode_hex("0337").unwrap(), &[0x03, 0x37]);
+        assert_eq!(decode_hex("337").unwrap(), &[0x03, 0x37]);
+        assert_eq!(decode_hex("0x0337").unwrap(), &[0x03, 0x37]);
+        assert_eq!(decode_hex("0x337").unwrap(), &[0x03, 0x37]);
+        assert_eq!(
+            decode_hex(
+                "0x4141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141"
+            )
+            .unwrap(),
+            &[
+                0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41,
+                0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41,
+                0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41,
+                0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41,
+            ]
+        );
+    }
+
+    #[test]
+    fn test_encode_hex() {
+        assert_eq!("1337", encode_hex(&[0x13, 0x37]));
+        assert_eq!("0337", encode_hex(&[0x03, 0x37]));
+        assert_eq!(
+                "4141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141",
+            encode_hex(
+            &[
+                0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41,
+                0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41,
+                0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41,
+                0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41,
+            ])
+        );
+    }
 }
