ecd5a67acd2aacfcb38449dba9661b19730ccc72
PlexSheep <software@cscherr.de>
2024-10-20 16:02:44 +0200

    feat(cli): read from stdin when the first argument is `-` #8

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Read;
 use std::path::PathBuf;
 
 use anyhow::{anyhow, Result};
@@ -9,12 +10,24 @@ fn main() -> Result<()> {
         eprintln!("! No JSON file was provided for the chalenge definition");
         return Err(anyhow!("no JSON file provided"));
     }
-    let path: PathBuf = args[1].clone().into();
-    eprintln!("* Path of the challenge definition: {:?}", path);
-    eprintln!("* Reading the challenge definition into memory");
 
-    let raw_text = std::fs::read_to_string(&path)
-        .inspect_err(|e| eprintln!("! Could not read the challenge definition file: {e}"))?;
+    let raw_text: String;
+
+    if args.len() == 2 && args[1] == "-" {
+        eprintln!("? Reading from stdin");
+        let mut buf: String = String::new();
+        let _len = std::io::stdin().read_to_string(&mut buf).inspect_err(|e| {
+            eprintln!("! Could not read the challenge definition from stdin: {e}");
+        })?;
+        raw_text = buf;
+    } else {
+        let path: PathBuf = args[1].clone().into();
+        eprintln!("* Path of the challenge definition: {:?}", path);
+        eprintln!("* Reading the challenge definition into memory");
+
+        raw_text = std::fs::read_to_string(&path)
+            .inspect_err(|e| eprintln!("! Could not read the challenge definition file: {e}"))?;
+    }
     let json_value: Value = serde_json::from_str(&raw_text).inspect_err(|e| {
         eprintln!("! Could not parse the text of the challenge definition file as JSON: {e}")
     })?;
