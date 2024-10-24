460e53bce4b2181fa8bc5bc293386fdc29b0af31
PlexSheep <software@cscherr.de>
2024-10-24 17:37:37 +0200

    chore: allow unreachable_code for debug actions
    sd_timeout does not return and count as unreachable code

--- src/challenge/debug.rs
+++ src/challenge/debug.rs
@@ -6,6 +6,7 @@ use crate::settings::Settings;
 
 use super::{Action, Testcase};
 
+#[allow(unreachable_code)]
 pub fn run_testcase(testcase: &Testcase, _settings: Settings) -> Result<serde_json::Value> {
     Ok(match testcase.action {
         Action::SD_Timeout => loop {
