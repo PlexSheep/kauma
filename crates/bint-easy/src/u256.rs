a270c8697e7eebfe468217d5c2a91fa6b0b53a13
PlexSheep <software@cscherr.de>
2024-10-31 21:47:12 +0100

    feat(bint): convert u256 to bytes

--- crates/bint-easy/src/u256.rs
+++ crates/bint-easy/src/u256.rs
@@ -4,13 +4,56 @@ use std::ops::{Add, Shl, Shr};
 
 use crate::bit_at_i;
 
-#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq, PartialOrd, Ord)]
+#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq, PartialOrd, Ord, Default)]
 pub struct U256(u128, u128);
 
 impl U256 {
+    pub const MAX: Self = U256(u128::MAX, u128::MAX);
+    pub const MIN: Self = U256(0, 0);
+    pub const BITS: u32 = 256;
+
     #[inline]
-    fn new(upper: u128, lower: u128) -> Self {
+    const fn new(upper: u128, lower: u128) -> Self {
         Self(upper, lower)
+    }
+
+    /// to native endian bytes
+    pub fn to_ne_bytes(self) -> [u8; 32] {
+        let mut buffer: [u8; 32] = [0; 32];
+        buffer[0..16]
+            .as_mut()
+            .copy_from_slice(&self.0.to_ne_bytes());
+        buffer[16..32]
+            .as_mut()
+            .copy_from_slice(&self.0.to_ne_bytes());
+
+        buffer
+    }
+
+    /// to big endian bytes
+    pub fn to_be_bytes(self) -> [u8; 32] {
+        let mut buffer: [u8; 32] = [0; 32];
+        buffer[0..16]
+            .as_mut()
+            .copy_from_slice(&self.0.to_be_bytes());
+        buffer[16..32]
+            .as_mut()
+            .copy_from_slice(&self.0.to_be_bytes());
+
+        buffer
+    }
+
+    /// to little endian bytes
+    pub fn to_le_bytes(self) -> [u8; 32] {
+        let mut buffer: [u8; 32] = [0; 32];
+        buffer[0..16]
+            .as_mut()
+            .copy_from_slice(&self.0.to_le_bytes());
+        buffer[16..32]
+            .as_mut()
+            .copy_from_slice(&self.0.to_le_bytes());
+
+        buffer
     }
 }
 
@@ -28,6 +71,7 @@ impl Add for U256 {
 
 impl Shl for U256 {
     type Output = Self;
+    // FIXME: only works for rhs==1
     fn shl(self, rhs: Self) -> Self::Output {
         let carry_bit = bit_at_i(self.1, 127);
         if carry_bit {
@@ -70,6 +114,18 @@ impl From<u8> for U256 {
     }
 }
 
+impl From<U256> for [u128; 2] {
+    fn from(value: U256) -> Self {
+        [value.0, value.1]
+    }
+}
+
+impl<'a> From<&'a U256> for [&'a u128; 2] {
+    fn from(value: &'a U256) -> Self {
+        [&value.0, &value.1]
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -106,3 +162,40 @@ mod test {
         assert_eq!(a << U256(0, 1), U256(1, u128::MAX << 1))
     }
 
+    // this test will fail on a big endian system
+    #[test]
+    fn test_u256_to_ne_bytes() {
+        let a = U256(255, 255);
+        assert_eq!(
+            a.to_ne_bytes(),
+            [
+                255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0,
+            ]
+        )
+    }
+
+    #[test]
+    fn test_u256_to_le_bytes() {
+        let a = U256(255, 255);
+        assert_eq!(
+            a.to_le_bytes(),
+            [
+                255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0,
+            ]
+        )
+    }
+
+    #[test]
+    fn test_u256_to_be_bytes() {
+        let a = U256(255, 255);
+        assert_eq!(
+            a.to_be_bytes(),
+            [
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 255,
+            ]
+        )
+    }
+}
